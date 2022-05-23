6ff5c4447bd6ff5568c4b909e5a1ed93cf02ac0a
Marc Schmitz <marc.schmitz@invision.de>
2022-05-23 20:13:57 +0200

    Pull out Dashboard

---
+++ marcmacs/layers/dashboard.el
@@ -0,0 +1,17 @@
+;;;; Startup with a custom Dashboard ;;;;
+(use-package dashboard
+  :ensure t
+  :demand
+  :init
+  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+  (setq dashboard-banner-logo-title "Happy Hacking!")
+  (setq dashboard-startup-banner "~/.emacs.d/marcmacs/monstermacs.png")
+  (setq dashboard-center-content t)
+  (setq dashboard-projects-backend 'projectile)
+  (setq dashboard-items
+	'((recents . 4)
+	  (projects . 5)
+	  (agenda . 4)))
+  (setq dashboard-set-file-icons t)
+  :config
+  (dashboard-setup-startup-hook))
