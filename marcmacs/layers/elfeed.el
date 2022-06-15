a57de1be87ab95789c68b8a0edeec0c51de840ef
Marc Schmitz <marc.schmitz@invision.de>
2022-06-15 17:21:02 +0200

    Use elfeed 🦾

---
+++ marcmacs/layers/elfeed.el
@@ -0,0 +1,14 @@
+;;;; Configure Elfeed & Co ;;;;
+(use-package elfeed
+  :init
+  (setq elfeed-feeds
+	'(
+	  ("http://nullprogram.com/feed/" blog)
+	  ("https://planet.emacslife.com/atom.xml" emacs blog)
+	  ("https://martinfowler.com/updates.atom" martinfowler updates)
+	  ("https://martinfowler.com/bliki/bliki.atom" martinfowler bliki)
+	  ("https://feed.infoq.com/RSS/articles/" innoq blog)
+	  ("https://feeds.simplecast.com/7y1CbAbN" maintainable podcast)
+	  ("https://innoq.podigee.io/feed/aac" innoq podcast)
+	  ("https://protesilaos.com/codelog.xml" patros blog)
+	  )))
