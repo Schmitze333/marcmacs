1a355c0f4cba501222ef9a235480b6b1007a6ffc
Marc Schmitz <marc.schmitz@invision.de>
2022-06-15 17:14:20 +0200

    Switch from bongo to emms 🎉

--- marcmacs/layers/entertainment.el
+++ marcmacs/layers/entertainment.el
@@ -1,2 +1,16 @@
-;;;; Play music ;;;;
-(use-package bongo)
+;;;; Entertainment Stuff ;;;;
+
+;;; Play all sorts of media ;;;
+(use-package mpv)
+(use-package emms
+  :config
+  (require 'emms-setup)
+  (emms-all)
+  (emms-default-players)
+  (setq emms-source-file-default-directory "~/Music/Musik"))
+
+;;; Show media info in the mode-line ;;;
+(use-package emms-mode-line-cycle
+  :after emms
+  :config
+  (emms-mode-line-mode t))
