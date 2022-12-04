71b91e02f5a219a8dd50279ccbcbbc9af042e866
Marc Schmitz <marc.schmitz@invision.de>
2022-12-04 22:19:10 +0100

    Adapt to new Mac

--- marcmacs/layers/macos.el
+++ marcmacs/layers/macos.el
@@ -4,5 +4,4 @@
 (setq mac-pass-command-to-system nil) ;; prevent OS defaults like CMD+h = hide
 (setq mac-command-modifier 'super)
 (setq mac-right-command-modifier 'meta)
-(setq mac-option-modifier 'meta)
-(setq mac-right-option-modifier nil)
+(setq mac-option-modifier nil)
