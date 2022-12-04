44d63aa72bb789d33d6d79737bb7ce09edec5c9a
Marc Schmitz <marc.schmitz@invision.de>
2022-12-04 22:20:43 +0100

    Fiddle with Supercollider

--- marcmacs/layers/supercollider.el
+++ marcmacs/layers/supercollider.el
@@ -1,12 +1,11 @@
-
 ;;;; Supercollider ;;;;
 
 ;; used to visit documentation
 ;; (use-package w3m)
 
-;; (use-package sclang
-;;   :load-path "/Users/marcschmitz/Library/Application Support/SuperCollider/downloaded-quarks/scel/el"
-;;   :init
-;;   (setq exec-path (append exec-path '("/Applications/SuperCollider.app/Contents/MacOS/"))))
-;; (use-package sclang-extensions)
-;; (use-package sclang-snippets)
+(use-package sclang
+  :load-path "/Users/marcschmitz/Library/Application Support/SuperCollider/downloaded-quarks/scel/el"
+  :init
+  (setq exec-path (append exec-path '("/Applications/SuperCollider.app/Contents/MacOS/"))))
+(use-package sclang-extensions)
+(use-package sclang-snippets)
